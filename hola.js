ca04e6f69ccce07e35ce85cfb799619416994775
joysg1 <joy.nelaton@utp.ac.pa>
2025-12-15 17:19:07 -0500

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,10 @@
+function saludar(mensaje) {
+    console.log(mensaje);
+}
+var mensaje = "¡Hola Mundo desde TypeScript en Manjaro!";
+saludar(mensaje);
+var usuario = {
+    nombre: "Carlos",
+    edad: 25
+};
+console.log("Usuario: ".concat(usuario.nombre, ", Edad: ").concat(usuario.edad));
